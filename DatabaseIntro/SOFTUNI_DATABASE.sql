941a12d9bc384f9d06f1635ef2d8ad5ca91dfa20
CecoSht329 <62944181+CecoSht329@users.noreply.github.com>
2021-01-12 17:32:28 +0200

    Add files via upload

--- DatabaseIntro/SOFTUNI_DATABASE.sql
+++ DatabaseIntro/SOFTUNI_DATABASE.sql
@@ -26,6 +26,59 @@ CREATE TABLE Employees(
 	  DepartmentId INT FOREIGN KEY REFERENCES Departments(Id) NOT NULL,
 	  HireDate DATE NOT NULL,
 	  Salary DECIMAL(7,2) NOT NULL,
-	  AddressId INT FOREIGN KEY REFERENCES Addresses(Id) NOT NULL
+	  AddressId INT FOREIGN KEY REFERENCES Addresses(Id) 
 )
 
+
+INSERT INTO Towns([Name])
+     VALUES
+	       ('Sofia'),
+		   ('Plovdiv'),
+		   ('Varna'),
+		   ('Burgas')
+
+
+INSERT INTO Departments([Name])
+     VALUES
+	       ('Engineering'),
+		   ('Sales'),
+		   ('Marketing'),
+		   ('Software Development'),
+		   ('Quality Assurance')
+
+INSERT INTO Employees(FirstName, MiddleName, LastName, JobTitle, DepartmentId, HireDate, Salary)
+     VALUES
+	      ('Ivan', 'Ivanov', 'Ivanov', '.NET Developer', 4, '02/01/2013', 3500.00),
+		  ('Petar', 'Petrov', 'Petrov', 'Senior Engineer', 1, '03/02/2004', 4000.00),
+		  ('Maria', 'Petrova', 'Ivanova', 'Intern', 5, '08/28/2016', 525.25),
+		  ('Georgi', 'Teziev', 'Ivanov', 'CEO', 2, '12/09/2007', 3000.00),
+		  ('Peter', 'Pan', 'Pan', 'Intern', 3, '08/28/2016', 599.88)
+
+--19.	Basic Select All Fields
+--20.	Basic Select All Fields and Order Them
+SELECT * FROM Towns
+ORDER BY [Name] ASC
+
+SELECT * FROM Departments
+ORDER BY [Name] ASC
+
+SELECT * FROM Employees
+ORDER BY Salary DESC
+
+--21.Basic Select Some Fields
+SELECT [Name] FROM Towns
+ORDER BY [Name] ASC
+
+SELECT [Name] FROM Departments
+ORDER BY [Name] ASC
+
+SELECT FirstName, LastName, JobTitle, Salary FROM Employees
+ORDER BY Salary DESC
+
+--22.Increase Employees Salary
+
+UPDATE Employees
+SET Salary += Salary * 0.1
+
+SELECT Salary FROM Employees
+ORDER BY Salary DESC
